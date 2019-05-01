ca5af2bfe69b7f876651d614f6f5850883356e13
Steven Montalbano <stevenmontalbano@gmail.com>
2019-04-30 20:08:44 -0400

    CPU redesign

---
+++ Programs/andEx.m
@@ -0,0 +1,12 @@
+v2.0 raw
+# µMIPS ISA version v0.9.3 03/20/19
+# to load this file into Logisim:
+# 1) save the output from the assembler to a file
+# 2) use the poke tool in Logisim and control-click the ROM/RAM component
+# 3) select Load Image menu option
+# 4) load the saved file
+9002
+9200
+1040
+7000
+6000
--- andEx.asm
+++
@@ -1,5 +0,0 @@
-li $r0, 1
-li $r1, 0
-and $r0, $r1
-put $r0
-halt
